3b7b2949154d76716ad008964b221f7d9aec1a4d
Mitchkal <mitchellkalenda@gmail.com>
2024-04-18 11:39:39 -0400

    added base64  retrieval

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -16,3 +16,17 @@ class BasicAuth(Auth):
         """
         super().__init__()
         pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        extracts the base64 part of Authorization header
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        encoded = authorization_header.split(' ')[1]
+        return encoded
