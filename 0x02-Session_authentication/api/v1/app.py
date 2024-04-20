515588cd4ecb07b7fcdfdafe966c06abef92b16f
Mitchkal <mitchellkalenda@gmail.com>
2024-04-20 07:57:42 -0400

    modified excluded paths

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -57,8 +57,7 @@ def before_request() -> str:
     request filtering
     """
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/', '/api/v1/auth_session/login/',
-                      '/api/v1/auth_session/logout/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth is None:
         return
 
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -32,7 +32,7 @@ class SessionAuth(Auth):
         user_id = self.user_id_by_session_id.get(session_id)
         return user_id
 
-    def current_user(self, request=None):
+    def current_user(self, request=None) -> str:
         """
         return User instance based on cookie value
         """
