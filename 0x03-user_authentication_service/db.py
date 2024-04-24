00fc6984f33ad4298835932cf2adaecb047f6520
Mitchkal <mitchellkalenda@gmail.com>
2024-04-24 15:04:15 -0400

    added method to find a user by arbitrary arguments

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base
 from user import User
@@ -44,3 +46,18 @@ class DB:
         self._session.commit()
 
         return (new_user)
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        find user by arbitrary arguments and returns
+        first row with user
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+
+        except NoResultFound:
+            raise NoResultFound
+
+        except InvalidRequestError as e:
+            raise InvalidRequestError
