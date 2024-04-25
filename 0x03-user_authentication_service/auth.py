d7d2f11339caeb14f08e2906d3a061a15b4ab6f7
Mitchkal <mitchellkalenda@gmail.com>
2024-04-24 21:12:16 -0400

    added method to create session id for a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,3 +75,17 @@ class Auth:
             return True
 
         return False
+
+    def create_session(self, email: str) -> str:
+        """
+        finds user corresponding to email,
+        generates new uuid,
+        and stores it as user's session id
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception as e:
+            pass
